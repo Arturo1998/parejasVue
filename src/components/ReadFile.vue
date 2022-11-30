88f058e247236e3c3af00bbbdc08696abd2aa6ad
Arturo1998 <amartinvaras@gmail.com>
2022-11-30 12:51:47 +0100

    primer commit

---
+++ src/components/ReadFile.vue
@@ -0,0 +1,25 @@
+<template>
+  <label>
+    {{ props.texto }}
+    <input type="file" @change="loadTextFromFile" />
+  </label>
+</template>
+
+<script setup>
+const props = defineProps({
+  texto: {
+    type: String,
+  },
+});
+
+const emits = defineEmits(["carga"]);
+
+const loadTextFromFile = (ev) => {
+  const file = ev.target.files[0];
+  const reader = new FileReader();
+  reader.onload = (e) => emits("carga", e.target.result);
+  reader.readAsText(file);
+};
+</script>
+
+<style></style>
